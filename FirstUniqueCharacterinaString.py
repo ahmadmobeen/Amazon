18b9e6252849f64c868871f875d322b8515b0c70
Mobeen Ahmad <ahmadmobeen24@gmail.com>
2022-06-22 15:28:35 +0900

    Create FirstUniqueCharacterinaString.py
    387

---
+++ FirstUniqueCharacterinaString.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/first-unique-character-in-a-string/
+
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+  
+        count = collections.Counter(s)
+        
+        for idx, ch in enumerate(s):
+            if count[ch] == 1:
+                return idx
+        return -1
+            
